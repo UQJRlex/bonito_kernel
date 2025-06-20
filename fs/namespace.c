83612dd0d1041c9848f811282d4833c3e9d8128b
UQJRlex <xuzhuyin527@gmail.com>
2025-06-20 09:13:15 +0800

    BRICKPORT: fs: refactor sys_umount to extract path_umount
    ref: https://github.com/backslashxx/msm8953-kernel/commit/d8603d0e0fac1ad21f6a3ad10af14b294bee4067

--- fs/namespace.c
+++ fs/namespace.c
@@ -1711,54 +1711,51 @@ static inline bool may_mandlock(void)
 }
 #endif
 
-/*
- * Now umount can handle mount points as well as block devices.
- * This is important for filesystems which use unnamed block devices.
- *
- * We now support a flag for forced unmount like the other 'big iron'
- * unixes. Our API is identical to OSF/1 to avoid making a mess of AMD
- */
-
-SYSCALL_DEFINE2(umount, char __user *, name, int, flags)
+int path_umount(struct path *path, int flags)
 {
-	struct path path;
-	struct mount *mnt;
+	struct mount *mnt = real_mount(path->mnt);
 	int retval;
-	int lookup_flags = 0;
 	bool user_request = !(current->flags & PF_KTHREAD);
 
-	if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
-		return -EINVAL;
+	if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW)) {
+		retval = -EINVAL;
+		goto dput_and_out;
+	}
+	
+	if (!may_mount()) {
+		retval = -EPERM;
+		goto dput_and_out;
+	}
 
-	if (!may_mount())
-		return -EPERM;
+	if (path->dentry != path->mnt->mnt_root) {
+		retval = -EINVAL;
+		goto dput_and_out;
+	}
 
-	if (!(flags & UMOUNT_NOFOLLOW))
-		lookup_flags |= LOOKUP_FOLLOW;
+	if (!check_mnt(mnt)) {
+		retval = -EINVAL;
+		goto dput_and_out;
+	}
+	
+	if (mnt->mnt.mnt_flags & MNT_LOCKED) { /* Check optimistically */
+		retval = -EINVAL;
+		goto dput_and_out;
+	}
 
-	retval = user_path_mountpoint_at(AT_FDCWD, name, lookup_flags, &path);
-	if (retval)
-		goto out;
-	mnt = real_mount(path.mnt);
-	retval = -EINVAL;
-	if (path.dentry != path.mnt->mnt_root)
+	if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN)) {
+		retval = -EPERM;
 		goto dput_and_out;
-	if (!check_mnt(mnt))
-		goto dput_and_out;
-	if (mnt->mnt.mnt_flags & MNT_LOCKED) /* Check optimistically */
-		goto dput_and_out;
-	retval = -EPERM;
-	if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))
-		goto dput_and_out;
+	}
 
 	/* flush delayed_fput to put mnt_count */
 	if (user_request)
 		flush_delayed_fput_wait();
 
 	retval = do_umount(mnt, flags);
+
 dput_and_out:
 	/* we mustn't call path_put() as that would clear mnt_expiry_mark */
-	dput(path.dentry);
+	dput(path->dentry);
 	if (user_request && (!retval || (flags & MNT_FORCE))) {
 		/* filesystem needs to handle unclosed namespaces */
 		if (mnt->mnt.mnt_sb->s_op->umount_end)
@@ -1783,6 +1780,22 @@ dput_and_out:
 	}
 out:
 	return retval;
+}
+
+SYSCALL_DEFINE2(umount, char __user *, name, int, flags)
+{
+	struct path path;
+	int lookup_flags = 0;
+	int ret;
+	
+	if (!(flags & UMOUNT_NOFOLLOW))
+		lookup_flags |= LOOKUP_FOLLOW;
+
+	ret = user_path_mountpoint_at(AT_FDCWD, name, lookup_flags, &path);
+	if (ret)
+		return ret;
+
+	return path_umount(&path, flags);
 }
 
 #ifdef __ARCH_WANT_SYS_OLDUMOUNT
